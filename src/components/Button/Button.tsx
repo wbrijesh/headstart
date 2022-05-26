d8836ed91653c08365c0c84e56a20a3c65969d83
Brijesh Wawdhane <brijesh@wawdhane.com>
2022-05-26 12:54:49 +0530

    setup tailwind production env not tested

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Button.css";
+import "../../styles/tailwind.css";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <button className="rounded-full border border-gray-500 px-4 py-1.5 text-sm text-gray-700">
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  enabled: process.env.NODE_ENV === "publish",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
